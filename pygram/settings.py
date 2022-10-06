761b1e2445679013a4a0be1cb7b6d142faffe9c1
Kris Payne <krispayne@me.com>
2022-10-06 13:45:41 -0500

    media file static continues

--- pygram/settings.py
+++ pygram/settings.py
@@ -154,7 +154,7 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [BASE_DIR / "static",]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = STATIC_ROOT / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
