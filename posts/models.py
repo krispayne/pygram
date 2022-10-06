a634f2feaeb1aaaf7ad188e8c7e0787ed11f4436
Kris Payne <krispayne@me.com>
2022-10-06 14:12:57 -0500

    add media dir

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    image = models.ImageField(upload_to='media')
+    image = models.ImageField(upload_to='images')
     description = models.TextField(null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
--- pygram/settings.py
+++ pygram/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -154,7 +155,7 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [BASE_DIR / "static",]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = ''
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
